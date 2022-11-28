420e40f028f8d726e1fa9a6f311155ef2b087bca
XuanKien-Nguyen <kienpc9x@gmail.com>
2022-11-28 14:47:23 +0700

    vue-router

---
+++ src/components/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>Not Found Page</div>
+</template>
+
+<script>
+export default {
+  name: 'NotFoundPage',
+}
+</script>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHashHistory } from 'vue-router'
+import App from './App.vue';
+import HomePage from './components/Home.vue';
+import LoginPage from './components/Login.vue';
+import RegisterPage from './components/Register.vue';
+import NotFoundPage from './components/NotFound.vue';
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/', name: 'Home', component: HomePage },
+    { path: '/login', name: 'Login', component: LoginPage },
+    { path: '/register', name: 'Register', component: RegisterPage },
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundPage },
+  ]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+  })
+
+createApp(App).use(router).mount('#app')
